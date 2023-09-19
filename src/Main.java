49b72cb499cfd83008cedf7755c82051b3eafb99
yeaokfr <chimariyanishanth@gmail.com>
2023-09-19 09:29:58 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter an integer:");
+        int num = scan.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("Your number is even");
+        }else{
+            System.out.println("Your number is odd");
+        }
     }
 }
